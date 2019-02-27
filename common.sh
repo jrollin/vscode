bb491274f4ecb781037540c77fed3fdbf158bcef
jrollin <1086565+jrollin@users.noreply.github.com>
2019-02-27 15:11:48 +0100

    add common and js scriots

---
+++ common.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+code --install-extension eg2.vscode-npm-script
+code --install-extension christian-kohler.npm-intellisense
+
+code --install-extension codezombiech.gitignore
+
+code --install-extension esbenp.prettier-vscode
+
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension oderwat.indent-rainbow
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension formulahendry.auto-close-tag
+
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension joaompinto.asciidoctor-vscode
+
+
---
+++ javascript.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+code --install-extension NuclleaR.vscode-extension-auto-import
+code --install-extension dbaeumer.vscode-eslint
+
+code --install-extension spoonscen.es6-mocha-snippets
+code --install-extension andys8.jest-snippets
+code --install-extension nwhatt.chai-snippets
+
+# react
+code --install-extension  planbcoding.vscode-react-refactor
+code --install-extension  burkeholland.simple-react-snippets
+
+
